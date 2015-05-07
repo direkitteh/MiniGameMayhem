5fde23ea53a0c7e58d17d59370dfb82ac0520e52
NumbuhFour <fad4470@rit.edu>
2015-05-07 16:28:27 -0400

    Added mouse selection to patterns

--- states/patterns/patternlevel.py
+++ states/patterns/patternlevel.py
@@ -33,6 +33,8 @@ class PatternLevel:
         selectedFont = pygame.font.SysFont(None, 32)
         selectedFont.set_underline(True)
         
+        mousePos = pygame.mouse.get_pos()
+        
         width = screen.get_width();
         height = screen.get_height();
         
@@ -48,18 +50,43 @@ class PatternLevel:
         shapePosY = height/2;
         for i in range(len(self.shapes)):
             posX = shapePosX + i*shapeSpacing
-            pygame.draw.rect(screen, [200,200,200],[posX-shapeSpacing/2+10, shapePosY-shapeSpacing/2+10, shapeSpacing-20,shapeSpacing-20]) # background
-            self.shapes[i].draw_shape(screen, [0,0,0], posX, shapePosY, 30) # TODO change color and width by selection
+            rect = [posX-shapeSpacing/2+10, shapePosY-shapeSpacing/2+10, shapeSpacing-20,shapeSpacing-20]
+            
+            shapeColor = [0,0,0]
+            bgColor = [200,200,200]
+            
+            if(self.shapeSelected == i):
+                shapeColor = [255,255,0]
+            if(mouse_in_rect(mousePos, rect)):
+                bgColor = [0,240,240]
+                if(pygame.mouse.get_pressed()[0] == True):
+                    self.shapeSelected = i
+            
+            pygame.draw.rect(screen, bgColor, rect) # background
+            self.shapes[i].draw_shape(screen, shapeColor, posX, shapePosY, 30) # TODO change color and width by selection
         
         
         for i in range(len(self.reasons)) :
-            res = font.render(self.reasons[i], True, [0,0,0]); # TODO change color by selection
             size = font.size(self.reasons[i]);
-            
             yPos = height/2 + 120 + i*(size[1]+15)
-            pygame.draw.rect(screen, [200,200,200], [0, yPos-3, width, size[1]+6]) # background
+            rect = [0, yPos-3, width, size[1]+6]
+            
+            textColor = [0,0,0]
+            bgColor = [200,200,200]
+            
+            if(self.reasonSelected == i):
+                textColor = [255,255,0]
+            if(mouse_in_rect(mousePos, rect)):
+                bgColor = [0,240,240]
+                if(pygame.mouse.get_pressed()[0] == True):
+                    self.reasonSelected = i
+            res = font.render(self.reasons[i], True, textColor); # TODO change color by selection
+            pygame.draw.rect(screen, bgColor, rect) # background
             screen.blit(res, [width/2 - size[0]/2, yPos])
         
         pygame.draw.line(screen, (0,0,0), (width/2,0), (width/2,height)) 
         pygame.draw.line(screen, (0,0,0), (0,height/2), (width,height/2))
         pass
+
+def mouse_in_rect(mousePos, rect):
+    return (mousePos[0] >= rect[0] and mousePos[1] >= rect[1] and mousePos[0] < (rect[0]+rect[2]) and mousePos[1] < (rect[1]+rect[3]))
