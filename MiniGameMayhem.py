312bdc993c5a5fd2dd386132d2eb989470d0d98b
milistisia2 <bxc4440@g.rit.edu>
2015-05-07 20:14:47 -0400

    added basic (incorrect) fraction generation. needs to be changed still

--- MiniGameMayhem.py
+++ MiniGameMayhem.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python
 import pygame
+import random
 from gi.repository import Gtk
+from fraction import Fraction
 
 
 class MiniGameMayhem:
+    #global variables
+    possibleX = [0,1,2,3,5,7,9,11,13,17,19,23]
+    possibleY = [1,2,3,5,7,9,11,13,17,19,23]
+    tempNumFracToPrint = 4
+    tempNumPrinted = 0;
+
     def __init__(self):
         # Set up a clock for managing the frame rate.
         self.clock = pygame.time.Clock()
@@ -72,9 +80,23 @@ class MiniGameMayhem:
             # Draw the ball
             pygame.draw.circle(screen, (255, 0, 0), (self.x, self.y), 100)
 
-            img=pygame.image.load("whiteboard.jpg")
-            screen.blit(img,(12,12))
-            img = pygame.transform.scale(img,(40,50))
+            #make the fraction
+            level = 1 #for now
+            while(self.tempNumPrinted < self.tempNumFracToPrint):
+                theFraction = self.makeFraction(level)
+                self.drawFraction(theFraction)
+                level = 2
+                theFraction = self.makeFraction(level)
+                self.drawFraction(theFraction)
+                level = 3
+                theFraction = self.makeFraction(level)
+                self.drawFraction(theFraction)
+                self.tempNumPrinted += 1
+                level = 1
+
+            #img=pygame.image.load("whiteboard.jpg")
+            #screen.blit(img,(12,12))
+            #img = pygame.transform.scale(img,(40,50))
             #print(type(img.get_width))
             #print("width = " + str(img.get_width()))
             #print("height = " + str(img.get_height()))
@@ -86,6 +108,45 @@ class MiniGameMayhem:
             # Try to stay at 30 FPS
             self.clock.tick(30)
 
+    def makeFraction(e, level):
+        #reference for possibleX/Y. See actual value at beginning of class
+        #possibleX = [0,1,2,3,5,7,9,11,13,17,19,23]
+        #possibleY = [1,2,3,5,7,9,11,13,17,19,23]
+        print("level is : " + str(level))
+        simpX = -1
+        simpY = -1
+        unSimpX = -1
+        unSimpY = -1
+        if (level == 1):
+            simpX = e.possibleX[random.randint(1,3)]
+            simpY = e.possibleY[random.randint(0,2)]
+            unSimpX = random.randint(2,3) * simpX
+            unSimpY = random.randint(2,3) * simpY
+        elif (level == 2):
+            simpX = e.possibleX[random.randint(0,6)]
+            simpY = e.possibleY[random.randint(0,5)]
+            unSimpX = random.randint(2,4) * simpX
+            unSimpY =  random.randint(2,4) * simpY
+        elif (level == 3):
+            simpX = e.possibleX[random.randint(4,7)]
+            simpY = e.possibleY[random.randint(4,7)]
+            unSimpX = random.randint(2,10) * simpX
+            unSimpY =  random.randint(2,10) * simpY
+        #create Fraction
+        theFraction = Fraction()
+        theFraction.simpX = simpX
+        theFraction.simpY = simpY
+        theFraction.unSimpX = unSimpX
+        theFraction.unSimpY = unSimpY
+        return theFraction
+
+    #TODO: finish this rather than just print the values
+    def drawFraction(e, theFraction):
+        print("theFraction.simpX = " + str(theFraction.simpX))
+        print("theFraction.simpY = " + str(theFraction.simpY))
+        print("theFraction.unSimpX = " + str(theFraction.unSimpX))
+        print("theFraction.unSimpY = " + str(theFraction.unSimpY))
+        pass
 
 # This function is called when the game is run directly from the command line:
 # ./TestGame.py
